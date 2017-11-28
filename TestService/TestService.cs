981592312096eba7791882e44ef6cf41c091fdd5
Ankit <33789321+AnkitSharma-007@users.noreply.github.com>
2017-11-28 11:51:50 +0530

    First commit

---
+++ TestService/TestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test.Services
+{
+    public class TestService
+    {
+        public bool IsEven(int value)
+        {
+            if (value%2==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
